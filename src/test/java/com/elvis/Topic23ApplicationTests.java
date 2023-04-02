8d43b77bd2afd86413dade8820c3f5ad0c876298
elvis-visi <sivisevi1@gmail.com>
2023-04-03 00:24:00 +0100

    8.Adding layout, using fragments and decorate

--- src/test/java/com/elvis/Topic23ApplicationTests.java
+++ src/test/java/com/elvis/Topic23ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Topic22ApplicationTests {
+class Topic23ApplicationTests {
 
 	@Test
 	void contextLoads() {
