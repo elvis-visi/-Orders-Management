711d0bf8121e5674026f79e024060e454c5bd8ed
elvis-visi <sivisevi1@gmail.com>
2023-04-03 17:46:14 +0100

    9. Renaming project.

--- src/test/java/com/elvis/Topic31ApplicationTests.java
+++ src/test/java/com/elvis/Topic31ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Topic23ApplicationTests {
+class Topic31ApplicationTests {
 
 	@Test
 	void contextLoads() {
