456aeeb37ab4ab5de2bf404be5103e84305aa65a
elvis-visi <sivisevi1@gmail.com>
2023-04-03 18:45:04 +0100

    11.1 Added dependency injection of the OrdersBusienssService.

--- src/main/java/com/elvis/controllers/OrdersController.java
+++ src/main/java/com/elvis/controllers/OrdersController.java
@@ -3,28 +3,29 @@ package com.elvis.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 import com.elvis.models.OrderModel;
+import com.elvis.services.OrdersBusinessService;
 
 @Controller
 @RequestMapping("/orders")
 public class OrdersController {
+	
+	//dependency injection. 
+	@Autowired
+	OrdersBusinessService service;
 
 	@GetMapping("/")
 	public String showAllOrders(Model model) {
 		
-		List<OrderModel> orders = new ArrayList<OrderModel>();
+		List<OrderModel> orders = service.getOrders();
 		
-		orders.add(new OrderModel(0L, "000","Sky diving exp", 1500.0f,1));
-		orders.add(new OrderModel(1L, "001","car", 1500.0f,1));
-		orders.add(new OrderModel(2L, "002","bike", 300.0f,111));
-		orders.add(new OrderModel(3L, "003","boat", 100.0f,1));
-		orders.add(new OrderModel(4L, "004","tennis", 4000.0f,2));
-		orders.add(new OrderModel(5L, "005","gym", 1500.0f,3));
+		
 		
 		model.addAttribute("title", "Here is what I want to do this summer");
 		model.addAttribute("orders", orders);
