631872415c857c7aae9b7e9f49bffaa80ba9d32a
elvis-visi <sivisevi1@gmail.com>
2023-04-03 22:46:32 +0100

    11.5 refactor name

--- src/main/java/com/elvis/Topic33Application.java
+++ src/main/java/com/elvis/Topic33Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Topic32Application {
+public class Topic33Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Topic32Application.class, args);
+		SpringApplication.run(Topic33Application.class, args);
 	}
 
 }
