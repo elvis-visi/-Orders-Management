5c0cbe688c41fcf2ef1d664e8ff08a109c6491bf
elvis-visi <sivisevi1@gmail.com>
2023-04-04 18:50:03 +0100

    14.2 Added OrdersDataService.

---
+++ src/main/java/com/elvis/data/OrdersDataService.java
@@ -0,0 +1,45 @@
+package com.elvis.data;
+
+import java.util.List;
+
+import com.elvis.models.OrderModel;
+
+public class OrdersDataService implements OrdersDataAccessInterface {
+
+	@Override
+	public OrderModel getById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderModel> getOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderModel> searchOrders(String searchTerm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long addOne(OrderModel newOrder) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean deleteOne(long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public OrderModel updateOne(long idToUpdate, OrderModel updateOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
