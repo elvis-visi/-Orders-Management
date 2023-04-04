fb02f30e7fdf99dad653038b59eb0f37b5adcaee
elvis-visi <sivisevi1@gmail.com>
2023-04-04 22:44:52 +0100

    15.3 Added Data Source and jdbcTemplate.

--- src/main/java/com/elvis/data/OrdersRepositoryInterface.java
+++ src/main/java/com/elvis/data/OrdersRepositoryInterface.java
@@ -1,5 +1,7 @@
 package com.elvis.data;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.elvis.models.OrderEntity;
@@ -7,7 +9,9 @@ import com.elvis.models.OrderEntity;
 public interface OrdersRepositoryInterface extends CrudRepository<OrderEntity,Long>{
 
 	// use the CrudRepository class in Spring to do the data operations on mysql
+	//already implies that we will use save, findall, findbyid, deletebyid etc.
 	
-	
+	//search 
+	List<OrderEntity> findbyProductNameContainingIgnoreCase(String searchTerm);
 	
 }
