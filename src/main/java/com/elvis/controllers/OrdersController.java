8fcbb62dac129946e23bbb1e18018a36c383af2d
elvis-visi <sivisevi1@gmail.com>
2023-04-05 13:22:12 +0100

    16.1 /addNew post mappng, add new order to DB.

--- src/main/java/com/elvis/controllers/OrdersController.java
+++ src/main/java/com/elvis/controllers/OrdersController.java
@@ -3,11 +3,15 @@ package com.elvis.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import com.elvis.models.OrderModel;
 import com.elvis.services.OrdersBusinessServiceInterface;
@@ -42,4 +46,39 @@ public class OrdersController {
 		
 	}
 	
+	@GetMapping("/showNewOrderForm")
+	public String showNewForm(Model model)
+	{
+		
+		model.addAttribute("order", new OrderModel());
+		
+		return "addNewOrderForm.html";
+	}
+	
+	
+
+	@PostMapping("/addNew")
+	public String addNew(@Valid OrderModel newOrder, BindingResult bindingResult, Model model )
+	{
+		
+		//id number should be null, as id is auto generated in the DB
+		newOrder.setId(null);
+		
+		//add to the database
+		service.addOne(newOrder);
+		
+		//get all orders from database
+		List<OrderModel> orders = service.getOrders();
+		
+		//show all orders page
+		model.addAttribute("orders", orders);
+		
+		return "orders";
+	}
+	
+	
+	
+	
+	
+
 }
