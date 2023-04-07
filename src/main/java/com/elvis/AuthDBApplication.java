b905667e5ea23ff51c622a90fe93ee08c4afdc12
elvis-visi <sivisevi1@gmail.com>
2023-04-07 14:06:53 +0100

    20. Securing a Web Application using a Database.

--- src/main/java/com/elvis/AuthDBApplication.java
+++ src/main/java/com/elvis/AuthDBApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApplication {
+public class AuthDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+		SpringApplication.run(AuthDBApplication.class, args);
 	}
 
 }
